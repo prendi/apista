9f620c3688e75e1672eea7c3243db26d646b8a74
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2016-02-28 17:21:53 +0000

    Added configuration files and execution scripts

---
+++ pt.iscte.apista.ant/extractAll.sh
@@ -0,0 +1,5 @@
+
+ant -DpropertiesPath='../pt.iscte.apista.resources/configSWT.properties' extract >> resultadoSWT.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configSpring.properties" extract >> resultadoSpring.txt
+
---
+++ pt.iscte.apista.ant/findAll.sh
@@ -0,0 +1,13 @@
+ant -DpropertiesPath='../pt.iscte.apista.resources/configJackson.properties' find > resultadoJackson.txt
+
+ant -DpropertiesPath='../pt.iscte.apista.resources/configJFreeChart.properties' find > resultadoJFreeChart.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configJSoup.properties" find > resultadoJSoup.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configMySQL.properties" find > resultadoMySQL.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configSecurity.properties" find > resultadoSecurity.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configSpring.properties" find > resultadoSpring.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configXML.properties" find > resultadoXML.txt
