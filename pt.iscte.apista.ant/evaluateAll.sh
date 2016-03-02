5961bce6b1595dd59331aefe5e00e7710bd95565
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2016-03-02 01:18:46 +0000

    Adding evaluation script

---
+++ pt.iscte.apista.ant/evaluateAll.sh
@@ -0,0 +1,10 @@
+ant -DpropertiesPath="../pt.iscte.apista.resources/configJSwing5.properties" evaluate >> resultsEvaluationTokensJSwing5.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configJSwing4.properties" evaluate >> resultsEvaluationTokensJSwing4.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configJSwing3.properties" evaluate >> resultsEvaluationTokensJSwing3.txt
+
+ant -DpropertiesPath="../pt.iscte.apista.resources/configJSwing2.properties" evaluate >> resultsEvaluationTokensJSwing2.txt
+
+
+
